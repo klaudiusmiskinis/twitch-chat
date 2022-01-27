7f4dfed2335e18e2d3cb7e604108c478402385d0
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2022-01-27 15:46:53 +0100

    Añadiendo condiciones para insertar iconos

--- app/index.js
+++ app/index.js
@@ -3,10 +3,23 @@ let cont = 0;
 let parar = false;
 
 socket.on('mensaje', (mensaje) => {
+    console.log(mensaje.nombre)
     let eleMensaje = '<div class="d-row mb-2" id="mensaje">' +
-                        '<div><b>@' + mensaje.nombre.username + '</b></div>' +
+                        '<div><b>' + mensaje.nombre.username + '</b></div>' +
                         '<div>' + mensaje.mensaje + '</div>' + 
                     '</div>';
+    if (mensaje.nombre.subscriber) {
+        eleMensaje = '<div class="d-row mb-2" id="mensaje">' +
+                            '<div><b><i class="bi bi-star-fill me-2 text-danger"></i>' + mensaje.nombre.username + '</b></div>' +
+                            '<div>' + mensaje.mensaje + '</div>' + 
+                        '</div>';
+    }
+    if (mensaje.nombre.mod) {
+        eleMensaje = '<div class="d-row mb-2" id="mensaje">' +
+                            '<div><b><i class="bi bi-wrench me-2 text-danger"></i>' + mensaje.nombre.username + '</b></div>' +
+                            '<div>' + mensaje.mensaje + '</div>' + 
+                        '</div>';
+    }
     let chat = document.getElementById('chat');
     chat.style.transition = 'all 1s cubic-bezier(0.1, 0.79, 0.49, 0.96) 0s'
     chat.insertAdjacentHTML('beforeend', eleMensaje);
@@ -21,8 +34,8 @@ document.getElementById('cambiar').onclick = function() {
     let nombre = document.getElementById('nombre').value;
     if(nombre.length > 0) {
         socket.emit('nombre', nombre);
-        let eleMensaje = '<div class="d-row mb-2 aviso">' +
-                        '<div><b> Se ha cambiado el canal a ' + nombre + '</b></div>' +
+        let eleMensaje = '<div class="d-row mb-2 aviso bg-color rounded p-1 px-2">' +
+                        '<div><b class="text-dark"> Se ha cambiado el canal a ' + nombre + '</b></div>' +
                        ' </div>';
         let chat = document.getElementById('chat');
         chat.insertAdjacentHTML('beforeend', eleMensaje);
